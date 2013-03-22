ce7ddeed291bf2b752215a869caccda6c06e7f3d
Ronny Wegener <wegener.ronny@gmail.com>
2013-03-22 21:04:22 +0100

    + updated makebreed

--- config-default.sh
+++ config-default.sh
@@ -17,14 +17,15 @@ BINFILE="$DISTROOT/usr/bin/$PKGNAME"
 
 CC="g++"
 CFLAGS="
+	-c
 	-Wall
 	-O2
 	$(wx-config --static=yes --debug=no --cflags)
 	"
 
 LD="g++"
-LDFLAGS="
-	-s
+LDFLAGS="-s"
+LDLIBS="
 	$(wx-config --static=yes --debug=no --libs --gl-libs)
 	-lavformat
 	-lavcodec
